f4c169b1fc84a6e9b1b863e50e0dda7629bbb2b0
Sergey Kvachonok <ravenexp@gmail.com>
2016-11-15 16:20:57 +0300

    parser: entity: Add wires and entity instances.
    The parser should be able to recognize very simple netlists now.
    
    Refactor entity port declaration grammar: always produce
    port direction by assuming inout ports.

--- parser/src/ast.rs
+++ parser/src/ast.rs
@@ -7,12 +7,14 @@ pub struct ModuleDecl {
 pub struct EntityDecl {
     pub name: Ident,
     pub endname: Ident,
-    pub ports: Option<Vec<PortDecl>>
+    pub ports: Option<Vec<PortDecl>>,
+    pub wires: Vec<WireDecl>,
+    pub insts: Vec<EntityInst>
 }
 
 pub struct PortDecl {
     pub name: Ident,
-    pub dir: Option<Direction>,
+    pub dir: Direction,
     pub class: Ident
 }
 
@@ -21,9 +23,21 @@ pub struct WireDecl {
     pub class: Ident
 }
 
+pub struct EntityInst {
+    pub name: Ident,
+    pub entity: Ident,
+    pub ports: Vec<PortAssign>
+}
+
+pub struct PortAssign {
+    pub port: Ident,
+    pub dir: Direction,
+    pub wire: Ident
+}
+
 // Terminals
 
-pub enum Direction { In, Out }
+pub enum Direction { None, In, Out }
 
 pub struct Number(pub String);
 pub struct Ident(pub String);
